2831d43e5d5123fea02af231edef7d138bbc1640
Matt McFarland <matmcfarland@gmail.com>
2012-12-20 17:08:35 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+/**
+ * Expose `Base64`
+ */
+
+var Base64 = module.exports = exports;
+
+/**
+ * Encode the given `str`
+ *
+ * @param {String} str ASCII representation
+ */
+
+Base64.encode = function(str) {
+  return new Buffer(str).toString('base64')
+              .replace(/=+$/, '')
+              .replace(/\//, '_')
+              .replace(/\+/g, '-');
+};
+
+  
+/**
+ * Decode the given `str`
+ *
+ * @param {String} str Base64 url safe representation
+ */
+
+Base64.decode = function(str) {
+  str = str.replace(/_/g, '/')
+           .replace(/-/g, '+');
+
+  return new Buffer(str, 'base64').toString('utf8');
+};
